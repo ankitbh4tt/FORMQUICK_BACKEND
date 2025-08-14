663fee1369ec9ca87aa1884d991a477c5cf849ec
ankitbh4tt <bhattankit152207@gmail.com>
2025-08-14 10:39:25 +0530

    add user cache and ai route

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,26 @@
-import express from "express";
-import cors from "cors";
-import errorHandler from "./middleware/errorHandler";
+import express from 'express';
+import cors from 'cors';
+import errorHandler from './middleware/errorHandler';
+import { clerkMiddleware } from '@clerk/express';
+import userRouter from './routes/user.routes';
+import clerkWebhookRouter from './routes/clerk.webhook';
+import cacheRouter from './routes/cache.routes';
+import aiRouter from './routes/ai.routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/ping", (_req, res) => {
-  return res
-    .status(200)
-    .json({ message: "Server is running perfectly fine!!!" });
+app.use(clerkMiddleware());
+
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/cache', cacheRouter);
+app.use('/api/v1/ai', aiRouter);
+app.use(clerkWebhookRouter);
+
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'Server is running perfectly fine!!!' });
 });
 
 app.use(errorHandler);
